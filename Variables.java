6d2b63c0290a181961e755b92c3bd31375ddd578
israelgame1763 <108910870+israelgame1763@users.noreply.github.com>
2023-09-19 20:45:07 -0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author expec
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad;
+        String nombre = "Israel"; // mi nombre
+        String materia = "Fundamentos de Programacion";
+        String nombreMascota = "borbuja";
+        final int añoNacimiento = 2005;
+        String cadenaTexto = "cadena de texto !!!!%%$$";
+        System.out.println("mi nombre es: "+nombre);
+        System.out.println("mi nombre es: "+nombre+" y estudio: "+materia);
+                ListaTareas listaYTareas = new ListaTareas();
+    }
+    
+}
